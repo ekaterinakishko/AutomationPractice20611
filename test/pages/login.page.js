bc92464ba912626bc88f63edafbbdd23e99e1053
akhunbabaeva <amcyuliya@gmail.com>
2020-08-25 19:31:14 -0700

    02

--- test/pages/login.page.js
+++ test/pages/login.page.js
@@ -1,23 +1,26 @@
 import BasePage from './base.page';
+const user = require ('../data/user');
 
 class LoginPage extends BasePage {
 
     get signInBtn () { return $('a.login') }
     get createAnAccount () { return $('#email_create') }
+    get createAnAccountBtn () {return $('#SubmitCreate') }
     get genderM () { return $('#id_gender1') }
     get genderF () { return $('#id_gender2') }
     get inputFirstName () { return $('#customer_firstname') }
     get inputLastName () { return $('#customer_lastname') }
+    get emailInput () {return $('#email')}
     get inputPassword () { return $('#passwd') }
-    get days () { return $$('#days')[8] }
-    get months () { return $$('#months')[5] }
-    get years () { return $$('#years')[1995] }
+    get days () { return $('#days') }
+    get months () { return $('#months') }
+    get years () { return $('#years') }
     get firstName () { return $('#firstname') }
     get lastName () { return $('#lastname') }
     get company () { return $('#company') }
     get address () { return $('#address1') }
     get city () { return $('#city') }
-    get state () { return $$('#id_state')[9] }
+    get state () { return $('#id_state.form-control')}
     get zipCode () { return $('#postcode') }
     get country () { return $('#id_country') }
     get addInformation () { return $('#other') }
@@ -27,14 +30,39 @@ class LoginPage extends BasePage {
     get header () { return $('.page-heading') }
 
 
+
     login (username, password) {
         this.inputUsername.setValue(username);
         this.inputPassword.setValue(password);
         this.btnSubmit.click();
     }
 
-    // days (n) {
-    //     return $$('#days')[n];
+    register (tempUser)//gender, firstName, lastName, email, password, month, day, year, company, address, city, state, zip, country, aI, phone, aliasEmail)
+     {
+        this.genderM.click();
+        this.inputFirstName.setValue(tempUser.firstName);
+        this.inputLastName.setValue(tempUser.lastName);
+        this.emailInput.setValue(tempUser.email);
+        this.inputPassword.setValue(tempUser.password);
+        browser.pause(2000)
+        this.days.selectByAttribute('value',tempUser.day);
+        this.months.selectByAttribute('value',tempUser.month);
+        this.years.selectByAttribute('value',tempUser.year);
+        this.firstName.setValue(tempUser.firstName);
+        this.lastName.setValue(tempUser.lastName);
+        this.company.setValue(tempUser.company);
+        this.address.setValue(tempUser.address);
+        this.city.setValue(tempUser.city);
+        this.state.selectByVisibleText(tempUser.state);
+        browser.pause(5000);
+        this.zipCode.setValue(tempUser.zipCode);
+        this.country.setValue(tempUser.country);
+        browser.pause(5000);
+        this.addInformation.setValue(tempUser.additionalInformation);
+        this.phone.setValue(tempUser.phone);
+        this.aliasEmail.setValue(tempUser.email);
+        this.btnSubmit.click();
+    }
 
     open () {
         return super.open('/');
